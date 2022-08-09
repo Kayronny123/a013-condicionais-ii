cc91be16cf385a8d2de479fb36c17bf49c57e36b
rogerio kayronny <kayronnysales@gmail.com>
2022-08-09 19:59:06 -0300

    Atualizando exercicio 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -4,35 +4,35 @@
 
 let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
-}
+// if(nacionalidade === "brasileira"){
+//     console.log("a pessoa é do Brasil!");
+// } else if(nacionalidade === "argentina"){
+//     console.log("a pessoa é da Argentina!");
+// } else if(nacionalidade === "uruguaia"){
+//     console.log("a pessoa é do Uruguai!");
+// } else if(nacionalidade === "chilena"){
+//     console.log("a pessoa é do Chile!");
+// } else if(nacionalidade === "colombiana"){
+//     console.log("a pessoa é da Colômbia!");
+// } else{
+//     console.log("nacionalidade não encontrada")
+// }
 
 switch(nacionalidade){
-    case nacionalidade:
-        console.log("Você é brasileiro(a)");
-        break;
-        case nacionalidade:
+        case "brasileiro":
+            console.log("Você é brasileiro(a)");
+            break;
+        case "argentino":
             console.log("Você é argentino(a)");
             break;
-            case nacionalidade:
+        case "uruguaio":
             console.log("Você é Uruguaio(a)");
-                break;
-                case nacionalidade:
-                console.log("Você é chileno(a)");
-                    break;
-                    case nacionalidade:
-                    console.log("Você é conlombiano(a))");
-                     default:
-                       console.log("Naconalidade não encontrada")
+            break;
+        case "chileno":
+            console.log("Você é chileno(a)");
+            break;
+        case "conlombiano":
+            console.log("Você é conlombiano(a))");
+        default:
+            console.log("Naconalidade não encontrada")
 }
