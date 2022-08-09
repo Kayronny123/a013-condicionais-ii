7cb11000aa5fbb46d0fe8a756d9efcdb5915d40b
rogerio kayronny <kayronnysales@gmail.com>
2022-08-09 15:17:54 -0300

    finalizado exercicio 2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,3 +1,7 @@
+// Recrie o exercício 3 da aula anterior (A012) de condicionais I, utilizando switch-case.
+//  No arquivo `script,js` existe uma versão do código já
+//  escrita, mas você pode colar o exercício que você fez na última prática!
+
 let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
 if(nacionalidade === "brasileira"){
@@ -12,4 +16,23 @@ if(nacionalidade === "brasileira"){
     console.log("a pessoa é da Colômbia!");
 } else{
     console.log("nacionalidade não encontrada")
+}
+
+switch(nacionalidade){
+    case nacionalidade:
+        console.log("Você é brasileiro(a)");
+        break;
+        case nacionalidade:
+            console.log("Você é argentino(a)");
+            break;
+            case nacionalidade:
+            console.log("Você é Uruguaio(a)");
+                break;
+                case nacionalidade:
+                console.log("Você é chileno(a)");
+                    break;
+                    case nacionalidade:
+                    console.log("Você é conlombiano(a))");
+                     default:
+                       console.log("Naconalidade não encontrada")
 }
